9d9052b688343ceb9362a9ed50f3d06d150b5056
Udaykiran8641 <142726275+Udaykiran8641@users.noreply.github.com>
2024-02-18 17:09:22 +0530

    Add files via upload
    DSA Problem 1

---
+++ DSA.java
@@ -0,0 +1,26 @@
+
+package FullDsa;
+import java.util.Scanner;
+public class DSA {
+    static int countDigit(int n){
+        int count=0;
+        while (n>0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+
+            System.out.println(countDigit(n));
+        }catch(Exception e){
+            System.out.println("Some Thing went wrong");
+        }
+    }
+
+}
